b57672dc66fe2da077e44887a8ff94014aa79987
fabioosnetto <fabioorione8@gmail.com>
2023-03-22 23:46:42 -0300

    2 new components
    Learning how to create a component, his classes, content...

---
+++ first_project/src/app/my-first/my-first.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+   selector: 'my-first-component',
+   template: `<span>, Fabio!</span>`
+})
+
+export class MyFirstComponent {}
---
+++ first_project/src/app/my-first2/my-first2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-first2-component',
+  templateUrl: './my-first2.component.html',
+  styleUrls: ['./my-first2.component.css']
+})
+export class MyFirst2Component {
+
+}
