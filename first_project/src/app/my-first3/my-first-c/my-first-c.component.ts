4e4575c4a66ab0275bece3cb39a6b17ad06b3931
fabioosnetto <fabioorione8@gmail.com>
2023-03-29 16:40:11 -0300

    ngFor learned

--- first_project/src/app/my-first3/my-first-c/my-first-c.component.ts
+++ first_project/src/app/my-first3/my-first-c/my-first-c.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-first-c',
   templateUrl: './my-first-c.component.html',
   styleUrls: ['./my-first-c.component.css']
 })
-export class MyFirstCComponent {
+export class MyFirstCComponent implements OnInit {
+  
+  folders: string[] = ['my-first3-m (that contains a module to exporting this component content to application on browser module)', 'my-first-c (that contains the component files)'];
+  
+  constructor(){
+  }
 
+  ngOnInit(){
+  }
 }
